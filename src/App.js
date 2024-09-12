ca97e79e4bea0116e26ad826cad890a5194eca7d
DSRCODE <daljeetkalyanwat62@gmail.com>
2024-09-12 20:28:35 +0530

    test commit

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1 style={{textAlign:"center", padding:"10px"}}>Happy Hackathon!!</h1>
+      <h1 style={{textAlign:"center", padding:"10px", color:"Red"}}>Happy Hackathon!!</h1>
     </div>
   );
 }
